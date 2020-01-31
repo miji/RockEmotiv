88d41288a9be70cb1fde2fd9f4e290a6346ff1e5
Miguel A. Teruel <mteruel@lucentialab.es>
2020-01-31 14:39:37 +0100

    First prototype

---
+++ Assets/LookAt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 1f;
+    private float offset_x, offset_z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset_x = transform.position.x - target.transform.position.x;
+        offset_z = transform.position.z - target.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(target.transform.position.x + offset_x, transform.position.y, target.transform.position.z + offset_z);
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Vector3 impulse = new Vector3(0.0f, 5.0f, 0.0f);
+    public float distance = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(impulse, ForceMode.Impulse);
+        }
+    }
+
+    
+}
